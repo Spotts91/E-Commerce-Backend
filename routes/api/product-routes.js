231521c4296610a57c1b69bb94fb4227cd60f54e
Spotts91 <oakshassa864@gmail.com>
2022-02-14 14:23:59 -0800

    Tentative final draft of product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -129,6 +129,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with that ID.'});
+        return;
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
